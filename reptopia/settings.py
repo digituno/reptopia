24a6d6f40a1481c61f760e78e51b5942d09dbd6b
reptopia <reptopia.kr@gmail.com>
2020-01-31 23:17:14 +0900

    change language code ko_kr > ko

--- reptopia/settings.py
+++ reptopia/settings.py
@@ -102,7 +102,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
-LANGUAGE_CODE = 'ko-KR'
+LANGUAGE_CODE = 'ko'
 
 TIME_ZONE = 'Asia/Seoul'
 
