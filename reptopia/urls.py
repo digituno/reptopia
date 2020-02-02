2376ea9d83d71e8efea7a12166034d78ba55253f
digituno <reptopia.kr@gmail.com>
2020-02-02 13:20:45 +0900

    로그인 페이지 추가 및 관련 urls 수정

--- reptopia/urls.py
+++ reptopia/urls.py
@@ -18,9 +18,11 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/login/', auth_views.LoginView.as_view(template_name='account/login.html'), name='login'),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', TemplateView.as_view(template_name='index.html')),
 ]
