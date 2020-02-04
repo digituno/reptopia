f24bd5b3c8c50517bd2696fdf0239bdad66bf11a
reptopia <reptopia.kr@gmail.com>
2020-02-04 15:24:31 +0900

    signup 파일 추가 등

--- account/views.py
+++ account/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView
+from django.contrib.auth.forms import UserCreationForm
+from django.core.urlresolvers import reverse_lazy
 
-# Create your views here.
+class SignupView(CreateView):
+    template_name = 'account/signup.html'
+    form_class = UserCreationForm
+    success_url = reverse_lazy('create_user_done')
+
+
+class SignupDoneView(TemplateView): # 회원가입이 완료된 경우
+    template_name = 'account/signup_done.html'
