7da8ff5e44e6b17193016e494b3b669d16f45290
digituno <reptopia.kr@gmail.com>
2020-02-04 21:02:59 +0900

    기본 auth 모듈을 이용한 회원가입 프로세스 등록

--- account/views.py
+++ account/views.py
@@ -7,7 +7,7 @@ from django.urls import reverse_lazy
 class SignupView(CreateView):
     template_name = 'account/signup.html.j2'
     form_class = UserCreationForm
-    success_url = reverse_lazy('create_user_done')
+    success_url = reverse_lazy('signup_done')
 
 
 class SignupDoneView(TemplateView):
