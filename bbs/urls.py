e5160696c249f7f548cee01e7db392257f870537
reptopia <reptopia.kr@gmail.com>
2020-02-21 15:00:30 +0900

    created post update view

--- bbs/urls.py
+++ bbs/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('post/', bbs_views.PostListView.as_view(), name='post-list'),
     path('post/add/', bbs_views.PostCreateView.as_view(), name='post-add'),
     path('post/<int:pk>/', bbs_views.PostDetailView.as_view(), name='post-detail'),
-    # path('post/<int:pk>/delete', views.PostDeleteView.as_view(), name='post-delete'),
-    # path('post/<int:pk>/update', views.PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete', bbs_views.PostDeleteView.as_view(), name='post-delete'),
+    path('post/<int:pk>/update', bbs_views.PostUpdateView.as_view(), name='post-update'),
 ]
--- bbs/views.py
+++ bbs/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import ListView, CreateView, DetailView
+from django.views.generic import ListView, CreateView, DetailView, DeleteView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.conf import settings
+from django.urls import reverse_lazy
 from .models import Post
 from .forms import PostCreateForm
 
@@ -31,6 +32,27 @@ class PostCreateView(LoginRequiredMixin, CreateView):
         return super().form_valid(form)
 
 
+class PostUpdateView(LoginRequiredMixin, UpdateView):
+    login_url = settings.LOGIN_URL
+    model = Post
+    form_class = PostCreateForm
+    template_name = 'bbs/post_form.html.j2'
+
+
 class PostDetailView(DetailView):
     model = Post
     template_name = 'bbs/post_detail.html.j2'
+
+
+class PostDeleteView(LoginRequiredMixin, DeleteView):
+    model = Post
+    success_url = reverse_lazy('post-list')
+
+    def get(self, request, *args, **kwargs):
+        return self.post(request, *args, **kwargs)
+
+    """
+    def get_queryset(self):
+        qs = super(PostDeleteView, self).get_queryset()
+        return qs.filter(pk=self.kwargs['pk'])
+    """
