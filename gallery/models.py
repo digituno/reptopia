36a7fb9aa5f78735ea7e66245456b5f7b12f94ca
digituno <reptopia.kr@gmail.com>
2020-02-22 21:23:40 +0900

    initialized gallery app

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.conf import settings
+from django.utils import timezone
+from django.urls import reverse
+import reptopia
 
-# Create your models here.
+
+class Photo(models.Model):
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='gallery/%Y/%m/%d')
+    desc = models.TextField(blank=True)
+    created_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return '[' + self.desc+ ']';
+
+    """
+    def get_absolute_url(self):
+        return reverse('pet-detail', kwargs={'userid': self.owner.pk, 'pk': self.pk})
+    """
+
+    def delete(self, *args, **kwargs):
+        self.image.delete()
+        super(Photo, self).delete(*args, **kwargs)
