c8b21d243c92b1b9c67495bc99cbb4c42901adb4
reptopia <reptopia.kr@gmail.com>
2020-03-13 13:42:37 +0900

    사육자 카드 클릭 시 사육자 목록 검색 결과 기능제공

--- home/urls.py
+++ home/urls.py
@@ -4,5 +4,6 @@ from . import views as home_views
 
 urlpatterns = [
     path('', home_views.IndexView.as_view(), name='index'),
+    path('cards/users', home_views.UserCardSearchView.as_view(), name='card-user'),
     # path('tag-autocomplete', home_views.TagAutocomplete.as_view(), name='tag-autocomplete'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from datetime import datetime
 # from dal import autocomplete
 from taggit.models import Tag
 from account.models import Account
 from pet.models import Pet, Care
+import reptopia
 import logging
 
 logger = logging.getLogger('reptopia.log')
@@ -27,6 +29,28 @@ class IndexView(TemplateView):
         context['account_count'] = Account.objects.count()
         context['pet_count'] = Pet.objects.count()
         context['care_count'] = Care.objects.count()
+
+        return context
+
+class UserCardSearchView(TemplateView):
+    model = Account
+    template_name = 'home/card_search.html.j2'
+
+    # custom user model 의 paginator 가 정상작동하지않음에 따른 직접구현
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        qs_all = Account.objects.all()
+        context['user_list'] = qs_all
+        paginator = Paginator(qs_all, reptopia._PAGE_CNT_*2)
+        page = self.request.GET.get('page')
+        try:
+            user_list = paginator.page(page)
+        except PageNotAnInteger:
+            user_list = paginator.page(reptopia._FIRST_PAGE_)
+        except EmptyPage:
+            user_list = paginator.page(paginator.num_pages)
+        context['user_list'] = user_list
 
         return context
 
